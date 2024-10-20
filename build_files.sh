7610529138b6e97ae58a9ea96080ff5f1809a08c
rohannso <rohannso14@gmail.com>
2024-10-21 03:11:17 +0530

    second

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,6 @@
-echo " BUILD START"
-python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic  --noinput --clear
-echo " BUILD END"
+#!/bin/bash
+echo "BUILD START"
+python3.9 -m pip install --upgrade pip  # Ensure pip is up-to-date
+python3.9 -m pip install -r requirements.txt  # Install requirements
+python3.9 manage.py collectstatic --noinput --clear  # Collect static files
+echo "BUILD END"
